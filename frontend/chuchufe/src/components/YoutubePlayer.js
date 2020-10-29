e7221d1bb0b3c8785d18f15ffc97a6205734eec3
niels <niels.overkamp@gmail.com>
2020-10-29 14:33:47 +0100

    uncomment commented line

--- frontend/chuchufe/src/components/YoutubePlayer.js
+++ frontend/chuchufe/src/components/YoutubePlayer.js
@@ -101,7 +101,7 @@ export default function YoutubePlayer({ playback, onSongEnd }) {
                 if (!window.YT) {
                     setIframeAPILoading(true);
                     const tag = document.createElement('script');
-                    // tag.src = 'https://www.youtube.com/iframe_api';
+                    tag.src = 'https://www.youtube.com/iframe_api';
                     const firstScriptTag = document.getElementsByTagName('script')[0];
                     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
                 } else {
